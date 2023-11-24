a9982317471167ecc925f75ddb5b5c8772b74d23
Garth Trickett <garthtrickett@gmail.com>
2023-11-24 12:05:19 +1100

    Added fullstack axiom logging

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,16 @@
 import React from 'react'
 import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
 import { gql } from '@apollo/client'
+import { log } from 'next-axiom'
 export const runtime = 'edge' // 'nodejs' is the default
+
+log.debug('Login attempt', { user: 'j_doe', status: 'success' }) // results in {"message": "Login attempt", "fields": {"user": "j_doe", "status": "success"}}
+log.info('Payment completed', { userID: '123', amount: '25USD' })
+log.warn('API rate limit exceeded', {
+  endpoint: '/users/1',
+  rateLimitRemaining: 0
+})
+log.error('System Error', { code: '500', message: 'Internal server error' })
 
 const QUERY = gql`
   query getTodos {
