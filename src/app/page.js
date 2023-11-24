c0a55dc1e5f6a7c6a60c63f2465fa6738ab1f27b
Garth Trickett <garthtrickett@gmail.com>
2023-11-24 12:49:50 +1100

    put back front end graphql

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,24 @@
 'use client'
 import React from 'react'
-// import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
-// import { gql } from '@apollo/client'
+import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
+import { gql } from '@apollo/client'
 export const runtime = 'edge' // 'nodejs' is the default
 
-// const QUERY = gql`
-//   query getTodos {
-//     todos {
-//       id
-//       description
-//       completed
-//     }
-//   }
-// `
+const QUERY = gql`
+  query getTodos {
+    todos {
+      id
+      description
+      completed
+    }
+  }
+`
 
 export const dynamic = 'force-dynamic'
 
 export default function Page() {
-  // const { data } = useSuspenseQuery(QUERY)
-  // console.log(data)
+  const { data } = useSuspenseQuery(QUERY)
+  console.log(data)
 
   return <div> hello</div>
 }
