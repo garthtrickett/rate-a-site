a99bc1990a6f10e344a12951f764da0c805dcb61
Garth Trickett <garthtrickett@gmail.com>
2023-11-24 12:10:51 +1100

    Added component logging with useLogger

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,9 @@
 import React from 'react'
 import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
 import { gql } from '@apollo/client'
-import { log } from 'next-axiom'
+// import { log } from 'next-axiom' // Use this for logging that happens outside a component
+import { useLogger } from 'next-axiom'
 export const runtime = 'edge' // 'nodejs' is the default
-
-log.debug('Login attempt', { user: 'j_doe', status: 'success' }) // results in {"message": "Login attempt", "fields": {"user": "j_doe", "status": "success"}}
-log.info('Payment completed', { userID: '123', amount: '25USD' })
-log.warn('API rate limit exceeded', {
-  endpoint: '/users/1',
-  rateLimitRemaining: 0
-})
-log.error('System Error', { code: '500', message: 'Internal server error' })
 
 const QUERY = gql`
   query getTodos {
@@ -27,6 +20,8 @@ export const dynamic = 'force-dynamic'
 
 export default function Page() {
   const { data } = useSuspenseQuery(QUERY)
+  const log = useLogger()
+  log.debug('User logged in', { userId: 42 })
   console.log(data)
 
   return <div> hello</div>
