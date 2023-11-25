e8d3df84ada4e993189e15aa8e6d1610c7b6b36b
Garth Trickett <garthtrickett@gmail.com>
2023-11-25 12:17:55 +1100

    wrapped apollo in clerk

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,18 +2,21 @@ import PropTypes from 'prop-types'
 import { ApolloWrapper } from '../lib/apollo-wrapper'
 import { AxiomWebVitals } from 'next-axiom'
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
 
 /**
  * @param {{ children: React.ReactNode }} props
  */
 function RootLayout({ children }) {
   return (
-    <ApolloWrapper>
-      <html lang="en">
-        <AxiomWebVitals />
-        <body>{children}</body>
-      </html>
-    </ApolloWrapper>
+    <ClerkProvider>
+      <ApolloWrapper>
+        <html lang="en">
+          <AxiomWebVitals />
+          <body>{children}</body>
+        </html>
+      </ApolloWrapper>
+    </ClerkProvider>
   )
 }
 
